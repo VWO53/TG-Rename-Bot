d3a5b2101c86d1891f83de0d107a2d6f81f89b68
VWO53 <64356181+VWO53@users.noreply.github.com>
2020-08-27 14:11:44 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -62,6 +62,8 @@ async def about_meh(bot, update):
 
 @Client.on_message(Filters.private & Filters.command("start") & Filters.text)
 async def start(bot,update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/start") 
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(update.from_user.first_name),
